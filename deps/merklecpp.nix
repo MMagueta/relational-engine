175a8decd3a7b64f7d33a43ef0d2a1240f930e5d
Marcos Magueta <maguetamarcos@gmail.com>
2024-10-27 01:40:10 -0300

    nix format

--- deps/merklecpp.nix
+++ deps/merklecpp.nix
@@ -2,19 +2,16 @@
 pkgs.stdenv.mkDerivation rec {
   name = "merklecpp";
 
- src = pkgs.fetchFromGitHub {
-         owner = "microsoft";
-         repo = "merklecpp";
-         rev = "master";
-         sha256 = "jJJ6FEn+AkMknqun0GkXh5DSXsJnFbdE7U8BQ7Dd/Fw=";
-       };
+  src = pkgs.fetchFromGitHub {
+    owner = "microsoft";
+    repo = "merklecpp";
+    rev = "master";
+    sha256 = "jJJ6FEn+AkMknqun0GkXh5DSXsJnFbdE7U8BQ7Dd/Fw=";
+  };
 
-  prePatch = ''
-  '';
-  configurePhase = ''
-  '';
-  buildPhase = ''
-  '';
+  prePatch = "";
+  configurePhase = "";
+  buildPhase = "";
   installPhase = ''
     mkdir -p $out/include
     mv merklecpp.h $out/include/merklecpp.h
--- deps/relational_engine_lib.nix
+++ deps/relational_engine_lib.nix
@@ -1,28 +1,24 @@
-{ pkgs, libressl ? pkgs.callPackage ./libressl.nix {}, merklecpp ? pkgs.callPackage ./merklecpp.nix {}, ... }:
-let
-  shared_lib_extension = if pkgs.stdenv.isDarwin then "dylib" else "so";
-in 
-pkgs.stdenv.mkDerivation {
+{ pkgs, libressl ? pkgs.callPackage ./libressl.nix { }
+, merklecpp ? pkgs.callPackage ./merklecpp.nix { }, ... }:
+let shared_lib_extension = if pkgs.stdenv.isDarwin then "dylib" else "so";
+in pkgs.stdenv.mkDerivation {
   name = "librelational_engine";
   src = ../shared/librelational_engine;
-  nativeBuildInputs = [
-    pkgs.cmake
-    pkgs.gcc
-  ];
+  nativeBuildInputs = [ pkgs.cmake pkgs.gcc ];
   buildInputs = [ libressl merklecpp ];
   env = {
-    MERKLECPP_INCLUDE_PATH="${merklecpp}/include";
-    LIBRESSL_INCLUDE_PATH="${libressl}/include";
-    LIBRESSL_LIB_PATH="${libressl}/lib";
+    MERKLECPP_INCLUDE_PATH = "${merklecpp}/include";
+    LIBRESSL_INCLUDE_PATH = "${libressl}/include";
+    LIBRESSL_LIB_PATH = "${libressl}/lib";
     OS_LIB_EXTENSION = shared_lib_extension;
   };
   buildPhase = ''
-      mkdir -p $out/lib
-      mkdir -p $out/include
-      cmake --build . --target relational_engine
+    mkdir -p $out/lib
+    mkdir -p $out/include
+    cmake --build . --target relational_engine
   '';
   installPhase = ''
-      mv librelational_engine.${shared_lib_extension} $out/lib/librelational_engine.${shared_lib_extension}
-      cp $src/library.h $out/include/library.h
+    mv librelational_engine.${shared_lib_extension} $out/lib/librelational_engine.${shared_lib_extension}
+    cp $src/library.h $out/include/library.h
   '';
 }
