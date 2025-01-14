c06ce5ea9c930ed800a8acb294ddceb7986fa5b4
Marinho <marinho.pinotti@gmail.com>
2025-01-13 22:26:24 -0300

    Format the ocaml code
    Also remove trailing whitespace from elisp client.

--- lib/core_functions.ml
+++ lib/core_functions.ml
@@ -5,19 +5,26 @@ module Types = Types_generated
 module Functions (F : Ctypes.FOREIGN) = struct
   (* open F *)
   open Ctypes_static
+
   let os_extension =
     if Sys.os_type = "Unix" then
       let ic = Unix.open_process_in "uname" in
       let uname = input_line ic in
       let () = close_in ic in
-      if uname = "Darwin" then
-        "dylib"
-      else "so"
+      if uname = "Darwin" then "dylib" else "so"
     else "dll"
+
   let libmerklecpp =
-    Dl.dlopen
-      ~flags:[RTLD_GLOBAL; RTLD_NOW]
-      ~filename:((Sys.getenv "LIBRELATIONAL_ENGINE_LIB_PATH") ^ "/librelational_engine." ^ os_extension)
-  let compute_hash = Foreign.foreign ~from:libmerklecpp "compute_hash" (ptr char @-> Ctypes.string @-> returning void)
-  let merkle_generate_root = Foreign.foreign ~from:libmerklecpp "merkle_generate_root" (ptr Ctypes.string @-> int @-> ptr char @-> returning void)
+    Dl.dlopen ~flags:[ RTLD_GLOBAL; RTLD_NOW ]
+      ~filename:
+        (Sys.getenv "LIBRELATIONAL_ENGINE_LIB_PATH"
+        ^ "/librelational_engine." ^ os_extension)
+
+  let compute_hash =
+    Foreign.foreign ~from:libmerklecpp "compute_hash"
+      (ptr char @-> Ctypes.string @-> returning void)
+
+  let merkle_generate_root =
+    Foreign.foreign ~from:libmerklecpp "merkle_generate_root"
+      (ptr Ctypes.string @-> int @-> ptr char @-> returning void)
 end
--- lib/protocol.ml
+++ lib/protocol.ml
@@ -1,10 +1,13 @@
 open Protocol_conv_xml
 open Sexplib0.Sexp_conv
-type tuple =
-  string list [@@deriving sexp, protocol ~driver:(module Xml_light)]
-type relation =
-  { attribute_name: string;
-    attribute_type: string;
-    tuples: tuple }
-    [@@deriving sexp, protocol ~driver:(module Xml_light)]
+
+type tuple = string list [@@deriving sexp, protocol ~driver:(module Xml_light)]
+
+type relation = {
+  attribute_name : string;
+  attribute_type : string;
+  tuples : tuple;
+}
+[@@deriving sexp, protocol ~driver:(module Xml_light)]
+
 type t = relation list [@@deriving sexp, protocol ~driver:(module Xml_light)]
